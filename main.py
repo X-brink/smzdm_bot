64a5bb61c72e4ad2c14638047258e95909bef93a
X-brink <iexiangshang@163.com>
2022-09-13 17:34:54 +0800

    fix:修复签到错误的问题

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """
 什么值得买自动签到脚本
 使用github actions 定时执行
-@author : stark
+@author : stark modify : brink
 """
-import requests,os
+import requests,os,time,json
 from sys import argv
 
 import config
@@ -17,15 +17,15 @@ class SMZDM_Bot(object):
 
     def __json_check(self, msg):
         """
-        对请求 盖乐世社区 返回的数据进行进行检查
+        对请求返回的数据进行进行检查
         1.判断是否 json 形式
         """
         try:
             result = msg.json()
-            print(result)
+            #print(result)
             return True
         except Exception as e:
-            print(f'Error : {e}')            
+            #print(f'Error : {e}')            
             return False
 
     def load_cookie_str(self, cookies):
@@ -34,17 +34,29 @@ class SMZDM_Bot(object):
         cookie 为浏览器复制来的字符串
         :param cookie: 登录过的社区网站 cookie
         """
-        self.session.headers['Cookie'] = cookies    
+        self.session.headers['Cookie'] = cookies
 
     def checkin(self):
         """
         签到函数
         """
         url = 'https://zhiyou.smzdm.com/user/checkin/jsonp_checkin'
-        msg = self.session.get(url)
+        params = {
+            "callback": "myCalback",
+            "_": int(time.time()*1000)
+        }
+        msg = self.session.get(url, params=params)
         if self.__json_check(msg):
             return msg.json()
-        return msg.content
+        msg.raise_for_status()
+        msg.encoding = msg.apparent_encoding
+        content = msg.text
+        data = json.loads(content[content.find("{"):content.rfind(")")])
+        if data.get("error_code") == 0:
+            res = "张大妈签到成功！！！总签到天数：" + data.get("data").get("checkin_num")
+        else:
+            res = "签到失败，原因：" + data.get("error_msg")
+        return res
 
 
 
@@ -63,4 +75,4 @@ if __name__ == '__main__':
         push_to_wechat(text = '什么值得买每日签到',
                         desp = str(res),
                         secretKey = SERVERCHAN_SECRETKEY)
-    print('代码完毕')
+    #print('代码完毕')
